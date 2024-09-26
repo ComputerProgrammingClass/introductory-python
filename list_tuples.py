bd7d28073012c76ff67e5e98525ce8c47c0adfb7
Eric Xiao <ericxiao0000@gmail.com>
2024-09-25 22:11:05 -0700

    feat: add list slices

--- list_tuples.py
+++ list_tuples.py
@@ -16,9 +16,17 @@ print(matthews[2])
 matthews[0] = "Poon"
 matthews[-1] = "Ho"
 
-# Len
+# Len()
 color = (53, 33, 0)
 matthews = ["Matthew 1", "Matthew 2", "Matthew 3", "Matthew 4", "Hahhaha"]
 
 print(len(color))
 print(len(matthews))
+
+# Slice
+teachers = ["Poon", "Peters", "Vi", "Yasui"]
+
+print(teachers)
+print(teachers[0])
+print(teachers[0:3])
+print(teachers[1:3])
